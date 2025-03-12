f4f8b96fa349ee956a7106527da13b2d92f79561
jolinefrisk <joline_frisk@hotmail.com>
2025-03-12 11:46:14 +0100

    fixed

---
+++ scr/main/mainTest.java
@@ -0,0 +1,13 @@
+package scr.main;
+public class mainTest{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("why work");
+    }
+
+    // Simple method to return a greeting message, which we will test
+    public String greet(String name) {
+        return "Hello " + name;
+    }
+
+}
--- scr/main/test.java
+++
@@ -1,8 +0,0 @@
-package scr.main;
-public class test{
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("why  work");
-    }
-
-}
